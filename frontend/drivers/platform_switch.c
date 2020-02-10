563b807395bcbf134b3bee1eec3f1471430bf49a
LightenPan <panliang188@gmail.com>
2020-02-10 22:34:28 +0800

    同步switch开发库版本更新

--- frontend/drivers/platform_switch.c
+++ frontend/drivers/platform_switch.c
@@ -708,7 +708,7 @@ static void frontend_switch_init(void *data)
    uint32_t width                = 0;
    uint32_t height               = 0;
 
-   nifmInitialize();
+   nifmInitialize(0);
    
    if(hosversionBefore(8, 0, 0))
       pcvInitialize();
